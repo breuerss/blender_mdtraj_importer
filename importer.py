646a7ae1129f4175636a0763362cfa21998833fb
Sebastian Breuers <sbreuers@gmail.com>
2016-07-19 17:49:55 +0200

    feature: add creation of material

--- importer.py
+++ importer.py
@@ -21,6 +21,7 @@
 # Export wrappers and integration with external tools.
 
 import bpy
+
 import mdtraj as md
 from mdtraj.core.element import *
 
@@ -54,8 +55,67 @@ def addKeyframesToMeshFromPositions (mesh, frames) :
         for frameIndex, frame in enumerate(frames):
             insert_keyframe(fcurves, frameIndex, frame[v.index]);
 
+colorMap = {
+        'hydrogen': (1, 1, 1, 1),
+        'nitrogen': (0.026, 0.322, 0.8, 1),
+        'carbon': (0.337, 0.378, 0.387, 1),
+        'oxygen': (0.8, 0.006, 0.037, 1),
+}
+
+def assignValuesToMaterial (material, properties):
+    for inputType, value in properties.items():
+        material.inputs[inputType].default_value = value
+
+def createMaterialForElement (element):
+    material = bpy.data.materials.new(name=element.name)
+    material.use_nodes = True
+
+    # Remove default
+    nodes = material.node_tree.nodes;
+    nodes.remove(nodes.get('Diffuse BSDF'))
+
+    # Create base color
+    material_baseColor = nodes.new('ShaderNodeBsdfToon')
+    assignValuesToMaterial(material_baseColor, {
+        'Color': colorMap[element.name],
+        'Size': 0.7,
+        'Smooth': 0.2
+        })
+
+    # Create rim color
+    material_rim = nodes.new('ShaderNodeBsdfToon')
+    assignValuesToMaterial(material_rim, {
+        'Color': (0, 0, 0, 1),
+        'Size': 0.7,
+        'Smooth': 0.2
+        })
+
+    # Create rim positioner
+    material_fresnel = nodes.new('ShaderNodeFresnel')
+    assignValuesToMaterial(material_fresnel, {
+        'IOR': 0.9
+        })
+
+    # Create mix node
+    material_mix = nodes.new('ShaderNodeMixShader')
+
+    # Bring them together
+    material_output = nodes.get('Material Output')
+    links = material.node_tree.links
+    links.new(material_output.inputs['Surface'], material_mix.outputs['Shader'])
+    links.new(material_mix.inputs[0], material_fresnel.outputs['Fac'])
+    links.new(material_mix.inputs[1], material_baseColor.outputs['BSDF'])
+    links.new(material_mix.inputs[2], material_rim.outputs['BSDF'])
+
+    return material;
+
+
 def getMaterialForElement (element):
-    return bpy.data.materials[element.name];
+    material = bpy.data.materials.get(element.name);
+    if not material:
+        material = createMaterialForElement(element)
+
+    return material;
 
 def getAtomRepresentation (element):
 
@@ -69,7 +129,7 @@ def getAtomRepresentation (element):
 
     ball.name = "Ball_" + element.name
     ball.active_material = getMaterialForElement(element);
-    
+
     return ball;
 
 def addObjectsToGroup (objectNames, groupName):
@@ -78,7 +138,7 @@ def addObjectsToGroup (objectNames, groupName):
         bpy.data.objects[name].select = True
 
     bpy.ops.group.create(name = groupName)
-    
+
 def createRepresentationForBlender(subsetTrajectory, element, createdObjects):
     print('Operate on element %s.' % element.name);
     indices = subsetTrajectory.topology.select('element %s' % element.symbol);
